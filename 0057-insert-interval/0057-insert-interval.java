9b11ae28d56316e8c9a65130823fc069c398efe9
Bhuvan30 <97089341+Bhuvan30@users.noreply.github.com>
2024-07-19 09:35:18 +0530

    Time: 1 ms (98.89%), Space: 44.6 MB (67.28%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        LinkedList<int[]> r=new LinkedList<>();
+        int i=0;
+        while(i<intervals.length && intervals[i][1]<newInterval[0]){
+            r.add(intervals[i]);
+            i++;
+        }
+        while(i<intervals.length && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(intervals[i][0],newInterval[0]);
+            newInterval[1]=Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        r.add(newInterval);
+        while(i<intervals.length){
+            r.add(intervals[i]);
+            i++;
+        }
+        return r.toArray(new int[r.size()][]);
+    }
+}
