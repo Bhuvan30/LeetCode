1b243012cef1e3428931257d1e1d5e04a75545e5
Bhuvan30 <97089341+Bhuvan30@users.noreply.github.com>
2025-01-21 18:38:28 +0530

    Time: 1 ms, Memory: 42 MB - LeetHub

--- 12424-1046-199-binary-tree-right-side-view/12424-1046-199-binary-tree-right-side-view.java
+++ 12424-1046-199-binary-tree-right-side-view/12424-1046-199-binary-tree-right-side-view.java
@@ -17,9 +17,9 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result=new ArrayList<>();
         if(root==null) return result;
-        Queue<TreeNode> q=new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while(q.size()>0){
+        while(q.size() > 0){
             int count = q.size();
             while(count-- > 0){
                 TreeNode val = q.remove();
